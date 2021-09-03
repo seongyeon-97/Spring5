c5918e0814c78b6b6df963d534bb71240a70202d
seongyeon-97 <creative9711@naver.com>
2021-09-03 17:44:56 +0900

    09/03
    Page 번호 추가한 수업

---
+++ src/test/java/com/sy/s5/DataSourceTest.java
@@ -0,0 +1,31 @@
+package com.sy.s5;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class DataSourceTest extends MyJunitTest{
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Test
+	public void test() throws Exception {
+		assertNotNull(dataSource.getConnection());
+	}
+	
+	@Test
+	public void sqlSessionTest() {
+		assertNotNull(sqlSession);
+	}
+	
+}
---
+++ src/test/java/com/sy/s5/MyJunitTest.java
@@ -0,0 +1,19 @@
+package com.sy.s5;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*-context.xml"})
+public abstract class MyJunitTest {
+	
+}
